cce70aa80cab84fd382a646b3a0cd17962aff309
Radu.Brehar <radu@jslog.com>
2015-05-23 00:33:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Live Markdown Preview
+
+## Install
+
+```sh
+$ npm i -g livemarked
+```
+
+## Usage
+
+```sh
+$ livemarked README.md 8080
+# livemarked <file> [<port>]
+```
+
+## Thanks
+
+Built on top of `socket.io`, `marked` and `express`.
+
+## LICENSE
+
+```MIT```
---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var path = require('path')
+var file = path.normalize(process.argv[2])
+
+var port = parseInt(process.argv[3], 10)
+
+if (isNaN(port)){
+    port = 3000
+}
+
+var marked = require('marked');
+// marked.setOptions({
+//   renderer: new marked.Renderer(),
+//   gfm: true,
+//   tables: true,
+//   breaks: false,
+//   pedantic: false,
+//   sanitize: true,
+//   smartLists: true,
+//   smartypants: false
+// })
+
+fs.watchFile(file, {interval: 50}, update)
+
+
+var sockets = []
+
+function update(){
+    var html = getHTML()
+
+    sockets.forEach(message.bind(this, html))
+}
+
+function message(msg, socket){
+    socket.emit('update', msg)
+}
+
+//start express and socket.io
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+function getHTML(){
+    var text = fs.readFileSync(file,'utf8')
+    console.log(text)
+    return marked(text)
+}
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('index.html'));
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected')
+  message(getHTML(), socket)
+  sockets.push(socket)
+
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
